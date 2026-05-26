004d5af8640181cc05ab7eafefaa894529116fea
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-26 18:49:56 +0545

    Feat: add other schemas

---
+++ backend/models/Expense.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    note: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Expense = mongoose.model("Expense", expenseSchema);
+export default Expense;
