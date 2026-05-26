3ba17952cba22429fa00decc7ce4ac3270ae4751
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-26 18:04:38 +0545

    feat: add verify email endpoint with other necessary packages

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,46 @@
 import User from "../models/User.js";
-
-export const register = (req, res) => {
+import crypto from "crypto";
+export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
+  // create new user
   const newUser = new User({ username, email, password });
-  newUser.save();
+
+  // Generate email verification token and save user
+  const verificationToken = crypto.randomBytes(20).toString("hex");
+  newUser.emailVerificationToken = verificationToken;
+  await newUser.save();
+
   res.status(201).json({
     success: true,
     message: "User registered successfully",
     data: newUser,
+  });
+};
+
+export const verifyEmail = async (req, res) => {
+  const { token } = req.params;
+
+  // Find user by verification token
+  const user = await User.findOne({ emailVerificationToken: token });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid or expired token" });
+  }
+
+  // Mark email as verified and clear the token
+  user.isVerified = true;
+  user.emailVerificationToken = undefined;
+
+  //save the user
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Email verified successfully",
+    data: user,
   });
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import asyncHandler from "../utils/asyncHandler.js";
-import { register } from "../controllers/auth.controller.js";
+import { register, verifyEmail } from "../controllers/auth.controller.js";
 const Router = express.Router();
 
 Router.post("/register", asyncHandler(register));
-
+Router.get("/verify-email/:token", asyncHandler(verifyEmail));
 export default Router;
