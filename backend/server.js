2febd372dac0d319622dd607c832856dcc736cc1
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-26 11:25:15 +0545

    feat: add db connection with user schema

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config({
+  path: "./.env.local",
+});
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
