ebd6a5e9c2a2f7ed8397bd747cd98557663addda
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-26 19:28:50 +0545

    feat: setup the main layout

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 bg-gray-800 text-white p-4 fixed h-full">Sidebar</div>
+  );
+};
+
+export default Sidebar;
